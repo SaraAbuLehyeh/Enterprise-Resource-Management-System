636a7c0a15aac49beb8ea8c06e223597799e8eb1
unknown <sarasuleiman8@gmail.com>
2025-04-02 20:36:48 +0200

    Add SQL SERVER Project named ERMS DATABASE

---
+++ ERMS.Database/ERMS.Database/Users.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+    [UserID] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    [FirstName] NVARCHAR(50) NOT NULL,
+    [LastName] NVARCHAR(50) NOT NULL,
+    [Email] NVARCHAR(100) NOT NULL,
+    [PasswordHash] NVARCHAR(128) NOT NULL,
+    [Role] NVARCHAR(20) NOT NULL,
+    [HireDate] DATETIME NOT NULL,
+    [DepartmentID] INT NOT NULL,
+    CONSTRAINT [FK_Users_Departments] FOREIGN KEY ([DepartmentID]) 
+        REFERENCES [dbo].[Departments]([DepartmentID])
+)
